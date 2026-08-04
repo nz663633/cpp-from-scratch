7a6f4f8d30777f95e2b530e046ea9b121abbd5f0
nz663633 <hyunjisim3@gmail.com>
2026-08-04 22:12:18 +0900

    feat: learn std::pow

--- Chapter1/operator.cpp
+++ Chapter1/operator.cpp
@@ -1,15 +1,20 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
 int main()
 {
     int x = 2; // x는 변수, 2는 정수 리터럴
-    
+
     // x가 0보다 크면 1, 아니면 2
     int y = (x > 0) ? 1 : 2; // 삼항 연산자
-
     cout << y << endl;
+
+    cout << "=====" << endl;
+
+    int z = std::pow(2, 3); // 2의 3제곱
+    cout << z << endl;
 
     return 0;
 }
