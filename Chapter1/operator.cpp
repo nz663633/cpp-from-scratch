85b95edf58ad891a29da2823d545ce4f1e48c0e8
nz663633 <hyunjisim3@gmail.com>
2026-08-05 21:00:03 +0900

    feat: learn increment/decrement, comma, and conditional operators

--- Chapter1/operator.cpp
+++ Chapter1/operator.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
 #include <cmath>
 
-using namespace std;
+int add(int a, int b)
+{
+    return a + b;
+}
 
 int main()
 {
+    using namespace std;
+
     int x = 2; // x는 변수, 2는 정수 리터럴
 
     // x가 0보다 크면 1, 아니면 2
@@ -15,6 +20,48 @@ int main()
 
     int z = std::pow(2, 3); // 2의 3제곱
     cout << z << endl;
+
+    cout << "=====" << endl;
+
+    int a = 6, b = 6;
+
+    cout << a << " " << b << endl;     // 6 6
+    cout << ++a << " " << --b << endl; // 7 5
+    cout << a++ << " " << b-- << endl; // 7 5
+    cout << a << " " << b << endl;     // 8 4
+
+    cout << "=====" << endl;
+
+    int i = 1, j = 2;
+    int k = add(i, ++j);
+
+    cout << k << endl;
+
+    cout << "=====" << endl;
+
+    int num1 = 3;
+    int num2 = 10;
+
+    // 쉼표 연산자
+    // int num3 = (++num1, ++num2);
+    ++num1;
+    ++num2;
+    int num3 = num2;
+
+    cout << num1 << " " << num2 << " " << num3 << endl;
+
+    cout << "=====" << endl;
+
+    // 조건부 연산자(3항 연산자)
+    bool onSale = true;
+    const int price = (onSale == true) ? 10 : 100;
+
+    // if (onSale)
+    //     price = 10;
+    // else
+    //     price = 100;
+
+    cout << "가격: " << price << endl;
 
     return 0;
 }
